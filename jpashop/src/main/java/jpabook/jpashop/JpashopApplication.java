59f1d7ae3fad7707fb5337a13ae62a11694164b4
Jin-Mook <jink0608@naver.com>
2022-07-27 07:59:22 +0900

    entity 생성 완료

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 
 		Hello hello = new Hello();
 		hello.setData("hello");
