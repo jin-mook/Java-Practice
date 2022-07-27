d407a93943c70b90ceebf985e24b6bd63f4c8a91
Jin-Mook <jink0608@naver.com>
2022-07-28 00:35:18 +0900

    주문 서비스 개발까지 진행

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -20,5 +20,5 @@ public class Delivery {
     private Address address;
 
     @Enumerated(EnumType.STRING)
-    private DeliveryStatus deliveryStatus;  // READY, COMP
+    private DeliveryStatus status;  // READY, COMP
 }
