57fe2d90638d08f1a0a9a28184d69acb499f533c
Jin-Mook <jink0608@naver.com>
2022-08-12 18:00:31 +0900

    spring mvc 강의 진행

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class MappingController {
+
+    @RequestMapping({"/hello-basic", "/hello-go"})
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userid = {}", data);
+        return "ok";
+    }
+}
