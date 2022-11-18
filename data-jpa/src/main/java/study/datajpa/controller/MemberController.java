7e0cefbdf25baa3bb336ec58cb935914111b4ce3
Jin-Mook <jink0608@naver.com>
2022-11-18 18:00:18 +0900

    엔티티 구별 방법

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -38,7 +38,7 @@ public class MemberController {
         return map;
     }
 
-    @PostConstruct
+//    @PostConstruct
     public void init() {
         for (int i = 0; i < 100; i++) {
             memberRepository.save(new Member("user" + i, i));
