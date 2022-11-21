f257a8bb1798ab24d6ffb7c95c34f976ebd5480c
Jin-Mook <jink0608@naver.com>
2022-11-21 16:36:44 +0900

    spring data jpa 마무리

---
+++ data-jpa/src/main/java/study/datajpa/repository/NestedClosedProjections.java
@@ -0,0 +1,13 @@
+package study.datajpa.repository;
+
+public interface NestedClosedProjections {
+
+    String getUsername();
+
+    TeamInfo getTeam();
+
+    interface TeamInfo {
+        String getName();
+    }
+
+}
