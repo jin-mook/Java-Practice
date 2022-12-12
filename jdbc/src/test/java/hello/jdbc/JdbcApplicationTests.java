6381e6ce8caeaf9d98fce341144d68a2a35acaba
Jin-Mook <jink0608@naver.com>
2022-12-12 18:02:06 +0900

    트랜잭션 처리

--- jdbc/src/test/java/hello/jdbc/JdbcApplicationTests.java
+++ jdbc/src/test/java/hello/jdbc/JdbcApplicationTests.java
@@ -1,10 +1,23 @@
 package hello.jdbc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class JdbcApplicationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	void context() {
+		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+		for (String beanDefinitionName : beanDefinitionNames) {
+			System.out.println("bean : " + beanDefinitionName);
+		}
+	}
 
 	@Test
 	void contextLoads() {
