f555f9579b7238e2494ccfcb280abe4596193998
Jin-Mook <jink0608@naver.com>
2023-02-02 18:05:49 +0900

    데코레이터패턴 예제코드 작성중

---
+++ proxy/src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,10 @@
+package hello.proxy.pureproxy.decorator.code;
+
+public class MessageDecorator implements Component{
+
+
+    @Override
+    public String operation() {
+        return null;
+    }
+}
