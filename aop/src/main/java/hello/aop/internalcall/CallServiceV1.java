6163febe49ed136490c1f001dca144d712725e6f
Jin-Mook <jink0608@naver.com>
2023-04-26 16:25:35 +0900

    스프링 aop - 실무 주의사항

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV1.java
@@ -0,0 +1,28 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    // 생성자 주입이 아닌 setter 주입을 이용해야 한다.
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        log.info("callServiceV1 setter={}", callServiceV1.getClass());
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external() {
+        log.info("call external");
+        callServiceV1.internal();
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
