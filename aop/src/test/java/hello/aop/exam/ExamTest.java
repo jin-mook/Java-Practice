1cc2296c986b58d43399055c0088b516a61ba975
Jin-Mook <jink0608@naver.com>
2023-04-26 14:05:42 +0900

    aop 실전 예제

---
+++ aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -0,0 +1,28 @@
+package hello.aop.exam;
+
+import hello.aop.exam.aop.RetryAspect;
+import hello.aop.exam.aop.TraceAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Import({TraceAspect.class, RetryAspect.class})
+@SpringBootTest
+class ExamTest {
+
+    @Autowired
+    ExamService examService;
+
+    @Test
+    void test() {
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i={}", i);
+            examService.request("data" + i);
+        }
+    }
+}
