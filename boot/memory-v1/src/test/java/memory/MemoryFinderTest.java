1e97cab8896caf0a4e6cd246b62df6407a44ac06
Jin-Mook <jink0608@naver.com>
2023-05-11 10:23:36 +0900

    memory-v1 build

--- boot/memory-v1/src/test/java/memory/MemoryFinderTest.java
+++ boot/memory-v1/src/test/java/memory/MemoryFinderTest.java
@@ -1,4 +1,19 @@
+package memory;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MemoryFinderTest {
-  
+
+    @Test
+    void get() {
+        MemoryFinder memoryFinder = new MemoryFinder();
+        Memory memory = memoryFinder.get();
+        System.out.println("memory = " + memory);
+        assertThat(memory).isNotNull();
+    }
+
 }
