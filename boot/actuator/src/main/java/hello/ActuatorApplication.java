5b1682ef78218de3afc5739a5e0f6800ddcef74f
Jin-Mook <jink0608@naver.com>
2023-05-15 16:32:02 +0900

    커스텀 metrics

--- boot/actuator/src/main/java/hello/ActuatorApplication.java
+++ boot/actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,11 +1,16 @@
 package hello;
 
+import hello.order.v0.OrderConfigV0;
+import hello.order.v1.OrderConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "hello.controller")
+//@Import(OrderConfigV0.class)
+@Import(OrderConfigV1.class)
 public class ActuatorApplication {
 
     public static void main(String[] args) {
